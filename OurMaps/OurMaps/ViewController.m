0def7bbe5f5b32421915531bb3ef941ba5a85fec
Jiangchuan <jiangchuan.huang@gmail.com>
2014-08-20 22:46:37 -0500

    Added call-out alert window.

--- OurMaps/OurMaps/ViewController.m
+++ OurMaps/OurMaps/ViewController.m
@@ -7,10 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <GoogleMaps/GoogleMaps.h>
 
-@interface ViewController (){
-    GMSMapView *mapView;
+@interface ViewController () <GMSMapViewDelegate> {
+//    GMSMapView *mapView;
 }
+@property (strong, nonatomic) GMSMapView *mapView;
 
 @end
 
@@ -19,18 +21,30 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:40.714353 longitude:-74.005973 zoom:15];
+	GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:40.714353
+                                                            longitude:-74.005973
+                                                                 zoom:15
+                                                              bearing:0
+                                                         viewingAngle:0];
 //    mapView = [GMSMapView mapWithFrame:CGRectZero camera:camera];
-    mapView = [GMSMapView mapWithFrame:self.view.bounds camera:camera];
-    mapView.myLocationEnabled = YES;
-    self.view = mapView;
+    self.mapView = [GMSMapView mapWithFrame:self.view.bounds camera:camera];
+    self.mapView.mapType = kGMSTypeNormal;
+    self.mapView.myLocationEnabled = YES;
+    self.mapView.settings.compassButton = YES;
+//    self.mapView.settings.myLocationButton = YES;
+    [self.mapView setMinZoom:10 maxZoom:18];
+    [self.view addSubview:self.mapView];
+    self.mapView.delegate = self;
+//    self.view = _mapView;
     
     // Show user location.
     GMSMarker *marker = [[GMSMarker alloc] init];
     marker.position = CLLocationCoordinate2DMake(40.714353, -74.005973);
     marker.title = @"Current Location";
     marker.snippet = @"New York City, USA";
-    marker.map = mapView;
+    marker.map = _mapView;
+    
+//    [self mapView:mapView didTapInfoWindowOfMarker:marker];
     
     // Show places around the user location.
     dispatch_async(dispatch_get_main_queue(), ^{
@@ -53,7 +67,7 @@
             marker.appearAnimation = kGMSMarkerAnimationPop;
 //            marker.icon = [GMSMarker markerImageWithColor:[UIColor greenColor]];
             marker.icon = [UIImage imageNamed:@"fig_Coffee.png"];
-            marker.map = mapView;
+            marker.map = _mapView;
         }
         
         // Google directions: get the route.
@@ -65,11 +79,35 @@
         GMSPolyline *polyline = [GMSPolyline polylineWithPath:path];
         polyline.strokeWidth = 4;
         polyline.strokeColor = [UIColor colorWithRed:0 green:0 blue:1.0 alpha:0.7];
-        polyline.map = mapView;
+        polyline.map = _mapView;
         
     });
     
     [GMSServices openSourceLicenseInfo];
+}
+
+-(BOOL) prefersStatusBarHidden{
+    return YES;
+}
+
+-(void) viewWillLayoutSubviews{
+    [super viewWillLayoutSubviews];
+    self.mapView.padding = UIEdgeInsetsMake(self.topLayoutGuide.length + 5,
+                                            0,
+                                            self.bottomLayoutGuide.length + 5,
+                                            0);
+}
+
+-(void)mapView:(GMSMapView *)mapView
+didTapInfoWindowOfMarker:(GMSMarker *)marker{
+    NSString *message = [NSString stringWithFormat:@"You tapped the info window for the %@ marker", marker.title];
+    UIAlertView *windowTapped = [[UIAlertView alloc] initWithTitle:@"info Window Tapped!"
+                                                           message:message
+                                                          delegate:nil
+                                                 cancelButtonTitle:@"Alright!"
+                                                 otherButtonTitles:nil];
+    
+    [windowTapped show];
 }
 
 - (void)didReceiveMemoryWarning
