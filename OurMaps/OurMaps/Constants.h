222927dc0dfc1ba833ed8945293a749f3539ea2c
luweiblues <weilu@Weis-MacBook-Pro.local>
2014-08-22 16:11:35 -0500

    constants added

---
+++ OurMaps/OurMaps/Constants.h
@@ -0,0 +1,13 @@
+//
+//  Constants.h
+//  OurMaps
+//
+//  Created by Wei Lu on 8/19/14.
+//  Copyright (c) 2014 OurMaps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Constants : NSObject
+
+@end
---
+++ OurMaps/OurMaps/Constants.m
@@ -0,0 +1,13 @@
+//
+//  Constants.m
+//  OurMaps
+//
+//  Created by Wei Lu on 8/19/14.
+//  Copyright (c) 2014 OurMaps. All rights reserved.
+//
+
+#import "Constants.h"
+
+@implementation Constants
+
+@end
