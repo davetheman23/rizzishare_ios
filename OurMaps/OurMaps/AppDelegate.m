38ffbbf34773e3d90da80b59cb1706a21cedf78d
luweiblues <luwei.blues@gmail.com>
2014-08-24 17:46:23 -0500

    minor changes

--- OurMaps/OurMaps/AppDelegate.m
+++ OurMaps/OurMaps/AppDelegate.m
@@ -10,6 +10,8 @@
 #import <GoogleMaps/GoogleMaps.h>
 #import "ViewController.h"
 #import <Parse/Parse.h>
+#import <FacebookSDK/FacebookSDK.h>
+#import <ParseFacebookUtils/PFFacebookUtils.h>
 
 @implementation AppDelegate
 
@@ -49,11 +51,18 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    [FBAppCall handleDidBecomeActiveWithSession:[PFFacebookUtils session]];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+    return [FBAppCall handleOpenURL:url sourceApplication:sourceApplication withSession:[PFFacebookUtils session]];
+}
+
 
 @end
